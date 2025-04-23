99398dee995d209c43aca857e23259f79bffe972
Gonzalo Delgado <mail@gonzalodelgado.com>
2025-04-23 15:21:25 -0300

    Primer commit.

---
+++ gotita.gd
@@ -0,0 +1,18 @@
+extends RigidBody2D
+
+var dir = 1
+var min_speed = 64
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var size = $CollisionShape2D.shape.radius
+	if abs(linear_velocity.x) < min_speed:
+		linear_velocity.x = min_speed * dir
+
+	if position.x + size <= 0 or position.x - size >= get_viewport_rect().size.x:
+		dir *= -1
