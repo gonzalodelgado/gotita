7faa45c6102bb81d8aa7cff7483e68bf476ec602
Gonzalo Delgado <mail@gonzalodelgado.com>
2025-05-08 08:29:52 -0300

    Pintar terreno.

--- level.gd
+++ level.gd
@@ -1,9 +1,12 @@
 extends StaticBody2D
 
+@onready var coll_polygon_2d = $CollisionPolygon2D
+@onready var polygon_2d = $CollisionPolygon2D/Polygon2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	polygon_2d.polygon = coll_polygon_2d.polygon
+	polygon_2d.offset = coll_polygon_2d.position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
