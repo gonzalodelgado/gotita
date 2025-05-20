7cb3eb4e830cd59a78f59899819c583d9bb678e8
Gonzalo Delgado <mail@gonzalodelgado.com>
2025-05-20 08:09:20 -0300

    Implementar carga dinámica de niveles.

--- juego.gd
+++ juego.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
+var indice_niveles: PackedStringArray
+const niveles_dir = "res://niveles"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	indice_niveles = ResourceLoader.list_directory(niveles_dir)
+	var current_level = ResourceLoader.load("%s/%s" % [niveles_dir, indice_niveles[0]]).instantiate()
+	add_child(current_level)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
