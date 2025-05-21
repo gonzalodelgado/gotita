47e1b4283f8145d49208cefa59cba9baebcf688c
Gonzalo Delgado <mail@gonzalodelgado.com>
2025-05-21 07:20:38 -0300

    Implementar secuencia básica de niveles.

--- juego.gd
+++ juego.gd
@@ -1,15 +1,27 @@
 extends Node2D
 
-var indice_niveles: PackedStringArray
+var indice_nivel: int = 0
+var nombres_niveles: PackedStringArray
+var nivel_actual
 const niveles_dir = "res://niveles"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	indice_niveles = ResourceLoader.list_directory(niveles_dir)
-	var current_level = ResourceLoader.load("%s/%s" % [niveles_dir, indice_niveles[0]]).instantiate()
-	add_child(current_level)
-
+	nombres_niveles = ResourceLoader.list_directory(niveles_dir)
+	cargar_nivel()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
+
+func cargar_nivel():
+	if is_instance_valid(nivel_actual):
+		remove_child(nivel_actual)
+		nivel_actual.queue_free()
+	nivel_actual = ResourceLoader.load("%s/%s" % [niveles_dir, nombres_niveles[indice_nivel]]).instantiate()
+	add_child(nivel_actual)
+	nivel_actual.gano_nivel.connect($SiguienteNivelTimer.start)
+
+func _on_siguiente_nivel_timer_timeout() -> void:
+	indice_nivel = (indice_nivel + 1) % nombres_niveles.size()
+	cargar_nivel()
--- level.gd
+++ level.gd
@@ -2,14 +2,20 @@ extends StaticBody2D
 
 @onready var coll_polygon_2d = $CollisionPolygon2D
 @onready var polygon_2d = $CollisionPolygon2D/Polygon2D
+@onready var screen_size = get_viewport_rect().size
 @export var max_gotitas = 10
 @export var gotitas_objetivo = 8
 @export var gotita_scene: PackedScene
 @export_enum("LIQUIDO", "GASEOSO", "SOLIDO") var estado_inicial: String
+
+enum Estados {PLAY, WIN, LOSE}
+@onready var estado: Estados = Estados.PLAY
 var gotitas_generadas = 0
 var gotitas_salvadas = 0
 var gotitas_perdidas = 0
 
+signal gano_nivel
+signal perdio_nivel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -21,17 +27,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	var screen_size = get_viewport_rect().size
+	if estado == Estados.PLAY and gotitas_salvadas >= gotitas_objetivo:
+		estado = Estados.WIN
+		gano_nivel.emit()
 
-	if gotitas_salvadas >= gotitas_objetivo:
-		$GanasteLabel.position.x = screen_size.x / 2 - $GanasteLabel.size.x / 2
-		$GanasteLabel.position.y = screen_size.y / 2 - $GanasteLabel.size.y / 2
-		$GanasteLabel.visible = true
-
-	if gotitas_perdidas > gotitas_objetivo:
-		$PerdisteLabel.position.x = screen_size.x / 2 - $PerdisteLabel.size.x / 2
-		$PerdisteLabel.position.y = screen_size.y / 2 - $PerdisteLabel.size.y / 2
-		$PerdisteLabel.visible = true
+	if estado == Estados.PLAY and gotitas_perdidas > gotitas_objetivo:
+		estado = Estados.LOSE
+		perdio_nivel.emit()
 
 func _on_timer_gotita_timeout() -> void:
 	if gotitas_generadas < max_gotitas:
@@ -48,3 +50,13 @@ func _on_objetivo_body_entered(body:Node2D) -> void:
 		body.queue_free()
 		gotitas_salvadas += 1
 		$"Objetivo/Tragar gota".play()
+
+func _on_gano_nivel() -> void:
+	$GanasteLabel.position.x = screen_size.x / 2 - $GanasteLabel.size.x / 2
+	$GanasteLabel.position.y = screen_size.y / 2 - $GanasteLabel.size.y / 2
+	$GanasteLabel.visible = true
+
+func _on_perdio_nivel() -> void:
+	$PerdisteLabel.position.x = screen_size.x / 2 - $PerdisteLabel.size.x / 2
+	$PerdisteLabel.position.y = screen_size.y / 2 - $PerdisteLabel.size.y / 2
+	$PerdisteLabel.visible = true
